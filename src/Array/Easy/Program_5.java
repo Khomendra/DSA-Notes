f11dc771e6a24b6117e2dc08401b8c54ab7cccce
Khomendra <khomendrakumar.ak.94@gmail.com>
2024-02-18 01:06:39 +0530

    left rotate an array by d position

---
+++ src/Array/Easy/Program_5.java
@@ -0,0 +1,38 @@
+//Example 1:
+//Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , left
+//Output: 3, 4, 5, 6, 7, 1, 2
+//Explanation: array is rotated to left by 2 position .
+
+package Array.Easy;
+
+//Left side : Brute force approach
+public class Program_5 {
+    public static void main(String []args){
+        //TC : O(d) + O(n-d) + O(d)
+        //SC : O(d)
+        System.out.println("Left rotate an array by D place");
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        int n = arr.length;
+        int d = 2;
+        leftRotate(arr, n, d);
+        for (int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void leftRotate(int []arr, int n, int d){
+        int tempArr[] = new int[n-d];
+
+        for (int i = 0; i < d; i++) {
+            tempArr[i] = arr[i]; //{1, 2}
+        }
+
+        for (int j = d; j < n; j++) {
+            arr[j-d] = arr[j]; //{3, 4, 5, 6, 7}
+        }
+
+        for (int k = n - d; k < n; k++) {
+            arr[k] = tempArr[k - (n-d)];
+        }
+    }
+}
